54dfc27957b604635e62d2ec85aaee5fbde96c58
Karan Gauswami <karangauswami.dev@gmail.com>
2020-12-17 10:19:48 +0530

    initial prototype

---
+++ examples/blocking.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+use freeswitch_esl::blocking::FreeswitchESL;
+use std::net::SocketAddr;
+
+fn main() -> Result<()> {
+    let addr = SocketAddr::from(([192, 168, 43, 222], 8021));
+    let mut esl = FreeswitchESL::new(addr, "ClueCon")?;
+    let response = esl.api("sofia status profile 192.168.43.222 reg")?;
+    eprintln!("response {:?}", response);
+    let response = esl.api("originate user/1002 1002")?;
+    eprintln!("response {:?}", response);
+    let response = esl.api("originate user/1001 1001")?;
+    eprintln!("response {:?}", response);
+    Ok(())
+}
