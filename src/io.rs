ee89036a08fd9395847350a2cc77fba752207548
KaranGauswami <karangauswami.dev@gmail.com>
2021-11-27 13:59:49 +0530

    proper filename

---
+++ src/io.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use bytes::Buf;
+use log::debug;
+use tokio_util::codec::{Decoder, Encoder};
+
+pub struct MyCodc {}
+impl MyCodc {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+impl Encoder<String> for MyCodc {
+    type Error = tokio::io::Error;
+    fn encode(&mut self, item: String, dst: &mut bytes::BytesMut) -> Result<(), Self::Error> {
+        debug!("self {}", item);
+        dst.extend_from_slice(item.as_bytes());
+        return Ok(());
+    }
+}
+#[derive(Debug)]
+pub enum Event {
+    Auth,
+    Reply(String),
+}
+impl Decoder for MyCodc {
+    type Item = Event;
+    type Error = anyhow::Error;
+    fn decode(&mut self, src: &mut bytes::BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        debug!("src is {:?}", src);
+        if src.starts_with(b"Content-Type: auth/request\n\n") {
+            src.advance(src.len());
+            Ok(Some(Event::Auth {}))
+        } else if src.starts_with(b"Content-Type: command/reply\nReply-Text: +OK accepted\n\n") {
+            src.advance(src.len());
+            let sts = String::from_utf8(src.to_vec());
+            Ok(Some(Event::Reply(sts.unwrap())))
+        } else {
+            src.advance(src.len());
+            debug!("{:?}", src);
+            Ok(None)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,96 @@
-use std::{io::BufRead, net::SocketAddr};
-
 use anyhow::Result;
 use futures::SinkExt;
+use log::debug;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::Duration;
 use tokio::net::TcpStream;
+use tokio::sync::Mutex;
 use tokio_stream::StreamExt;
-use tokio_util::codec::{Decoder, Encoder, Framed, FramedRead};
+use tokio_util::codec::Framed;
 
-struct Karan {}
-impl Karan {
-    fn new() -> Self {
-        Self {}
-    }
-}
+mod io;
+
+use tokio::sync::mpsc::channel;
+use tokio::sync::mpsc::Sender;
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let addr: SocketAddr = "3.109.206.34:8021".parse().unwrap();
-    let stream = TcpStream::connect(addr).await?;
-    let my_coded = Karan::new();
-    let mut transport = Framed::new(stream, my_coded);
-    let mut counter = 0;
-    while let Some(frames) = transport.next().await {
-        println!("lol");
-        println!("{:?}", frames);
-        if let Ok(something) = frames {
-            match something {
-                Event::Auth => {
-                    println!("got auth");
-                    let _ = transport.send("auth ClueCon\n\n".to_string()).await;
-                    let _ = transport.next().await;
-                }
-                Event::Reply(n) => {
-                    println!("got reply {}", n);
-                    break;
-                }
-            }
-        }
-    }
+    env_logger::init();
+
+    let addr = "3.109.206.34:8021".parse().unwrap();
+    let inbound = Inbound::new(addr).await?;
+    let _ = inbound.api("reloadxml").await;
+    let _ = inbound.api("sofia profile external restart").await;
+    let _ = inbound.api("sofia profile external restart").await;
+    debug!("finished");
+    tokio::time::sleep(Duration::from_secs(10)).await;
     Ok(())
 }
-impl Encoder<String> for Karan {
-    type Error = tokio::io::Error;
-    fn encode(&mut self, item: String, dst: &mut bytes::BytesMut) -> Result<(), Self::Error> {
-        println!("self {}", item);
-        dst.extend_from_slice(b"auth ClueCon\r\n\r\n");
-        return Ok(());
+pub struct Inbound {
+    sender: Arc<Sender<String>>,
+    commands: Arc<Mutex<Vec<Sender<String>>>>,
+}
+
+use crate::io::{Event, MyCodc};
+
+impl Inbound {
+    pub async fn receive(&mut self) {}
+    pub async fn new(socket: SocketAddr) -> Result<Self, tokio::io::Error> {
+        let stream = TcpStream::connect(socket).await?;
+        let (sender, mut receiver) = channel(1);
+        let sender = Arc::new(sender);
+        let commands = Arc::new(Mutex::new(vec![]));
+        let inner_commands = Arc::clone(&commands);
+        let connection = Self { sender, commands };
+        let my_coded = MyCodc::new();
+        let mut transport = Framed::new(stream, my_coded);
+        debug!("will read one frame");
+        let _ = transport.next().await.unwrap().unwrap();
+        debug!("read one frame");
+        let _ = transport.send("auth ClueCon\n\n".to_string()).await;
+        let _ = transport.next().await.unwrap().unwrap();
+        tokio::spawn(async move {
+            loop {
+                tokio::select! {
+
+
+                    frame = receiver.recv() => {
+                        if let Some(message) = frame {
+                            debug!("writing command : {}",message);
+                            let _ = transport.send(message).await;
+                        }
+                    },
+                    something = transport.next() => {
+                        match something.unwrap().unwrap() {
+                            Event::Auth => {
+                                debug!("got auth");
+                                let _ = transport.send("auth ClueCon\n\n".to_string()).await;
+                            }
+                            Event::Reply(n) => {
+                                let tx = inner_commands.lock().await.pop().unwrap();
+                                debug!("got reply {}", n);
+                                let _ = tx.send(n.clone()).await;
+                                debug!("send channel data for {}",n);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        Ok(connection)
     }
-}
-#[derive(Debug)]
-enum Event {
-    Auth,
-    Reply(String),
-}
-impl Decoder for Karan {
-    type Item = Event;
-    type Error = anyhow::Error;
-    fn decode(&mut self, src: &mut bytes::BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        if src.starts_with(b"Content-Type: auth/request\n\n") {
-            src.clear();
-            Ok(Some(Event::Auth {}))
-        } else if src.starts_with(b"Content-Type: command/reply\nReply-Text: +OK accepted\n\n") {
-            let sts = String::from_utf8(src.to_vec());
-            Ok(Some(Event::Reply(sts.unwrap())))
+    async fn api(&self, command: &str) -> Result<()> {
+        debug!("Send api");
+        self.sender.send(format!("api {}\n\n", command)).await?;
+        let (sender, mut receiver) = channel(10);
+        self.commands.lock().await.push(sender);
+        // commands.push(sender);
+        if let Some(a) = receiver.recv().await {
+            debug!("received data from channel: {}", a);
+            Ok(())
         } else {
-            println!("{:?}", src);
-            Ok(None)
+            Ok(())
         }
     }
 }
